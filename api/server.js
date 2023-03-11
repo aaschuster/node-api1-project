6f9ae83609599666b67911c6987fa578c2777527
Aaron Schuster <aaronjschuster@gmail.com>
2023-03-11 12:53:09 -0500

    set up initial server

--- api/server.js
+++ api/server.js
@@ -1,3 +1,13 @@
 // BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+    res.status(200).json({ message: "this is a test"});
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const server = require('./api/server');
+const server = require('./api/server.js');
 
 const port = 9000;
+
+server.listen(9000, () => {
+    console.log(`server running at port ${port}`);
+})
 
 // START YOUR SERVER HERE
