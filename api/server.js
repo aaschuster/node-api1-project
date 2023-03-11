3384cfeb39c011917e5466f6c37a3c9fa303eda4
Aaron Schuster <aaronjschuster@gmail.com>
2023-03-11 13:02:09 -0500

    build out endpoint to add new users

--- api/server.js
+++ api/server.js
@@ -2,12 +2,36 @@
 
 const express = require("express");
 
+const User = require("./users/model.js")
+
 const server = express();
 
 server.use(express.json());
 
 server.get("/", (req, res) => {
     res.status(200).json({ message: "this is a test"});
+})
+
+server.post("/api/users", async (req, res) => {
+    try {
+        const { name, bio } = req.body;
+
+        if(!name || !bio ) {
+            res.status(422).json( {
+                message: "Name and Bio are both required"
+            })
+        } else {
+            const newUser = await User.insert(req.body);
+            res.status(201).json( {
+                message: "New user successfully created",
+                data: newUser
+            })
+        }
+    } catch (err) {
+        res.status(500).json( {
+            message: `Something went wrong... ${err}`
+        })
+    }
 })
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
