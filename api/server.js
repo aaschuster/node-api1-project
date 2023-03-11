2df383e0eea657c3cab24e9bc4c63dea109131f1
Aaron Schuster <aaronjschuster@gmail.com>
2023-03-11 14:05:46 -0500

    build out endpoint to update users

--- api/server.js
+++ api/server.js
@@ -62,6 +62,35 @@ server.post("/api/users", async (req, res) => {
     }
 })
 
+server.put("/api/users/:id", async (req, res) => {
+    try {
+        const { name, bio } = req.body;
+        const {id} = req.params;
+
+        if(!name || !bio) {
+            res.status(422).json( {
+                message: "Name and Bio are both required"
+            })
+        } else {
+            const updatedUser = await User.update(id, req.body);
+            if(!updatedUser) {
+                res.status(404).json( {
+                    message: `Can't find user with ID of ${id}`
+                })
+            } else {
+                res.status(201).json( {
+                    message: "User successfully updated",
+                    data: updatedUser
+                })
+            }
+        }
+    } catch (err) {
+        res.status(500).json( {
+            message: `Something went wrong... ${err}`
+        })
+    }
+})
+
 server.delete("/api/users/:id", async (req, res) => {
     try {
         const {id} = req.params;
