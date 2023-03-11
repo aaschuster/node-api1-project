1ba132b01d51192426ca2c914987a54d646e5f41
Aaron Schuster <aaronjschuster@gmail.com>
2023-03-11 13:13:07 -0500

    build out endpoint to get user by ID

--- api/server.js
+++ api/server.js
@@ -23,6 +23,23 @@ server.get("/api/users", async (req, res) => {
     }
 })
 
+server.get("/api/users/:id", async (req, res) => {
+    try {
+        const {id} = req.params;
+
+        const user = await User.findById(id);
+        if(!user) 
+            res.status(404).json({ 
+                message: `Can't find user with ID of ${id}`
+            })
+        else res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json( {
+            message: `Something went wrong... ${err}`
+        })
+    }
+})
+
 server.post("/api/users", async (req, res) => {
     try {
         const { name, bio } = req.body;
